e5a99a38ae74f1b78a26ab9d68f10ecd985e9d3e
tugot17 <piotr635@gmail.com>
2020-10-23 15:11:32 +0200

    Sentence tokenizer added

--- sentence_tokenizer.py
+++ sentence_tokenizer.py
@@ -6,4 +6,21 @@ def get_sentences_from_text(text):
     :param text: string, e.g. "I go to school. My school is a decent place to go to, unlike yours"
     :return: List[string], e.g. ["I go to school", "My school is a decent place to go to, unlike yours"]
     """
-    pass
+    regex = r"(?<=[.!?])\s+(?=[A-Z])"
+
+    # subst = "\\n\\n"
+
+    # matches = re.findall(regex, text)
+    matches = re.finditer(regex, text, re.MULTILINE)
+
+    subst = "\\n\\n"
+    subst = ""
+
+    result = re.sub(regex, subst, text, 0, re.MULTILINE)
+
+    return result, list(matches)
+
+if __name__ == '__main__':
+    text = "I go to school. My school is a decent place to go to, unlike yours"
+
+    print(get_sentences_from_text(text))
