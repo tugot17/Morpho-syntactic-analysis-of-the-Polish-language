afe3fce7effc83055d5d03bb0750f3173259493b
tugot17 <piotr635@gmail.com>
2020-10-23 14:54:23 +0200

    Word tokenizer, code added

--- word_tokenizer.py
+++ word_tokenizer.py
@@ -1,9 +1,19 @@
 import re
 
-def get_words_from_sentence(sentence):
+def get_words_from_sentence(text):
     """
     Function returns a list of words for a given sentence
-    :param sentence: string, e.g. "etc. I go to school"
-    :return: List[string], e.g. ["etc.", "I", "go" "to, "school"]
+    :param text: string, e.g. "etc. I go to school. I really like it."
+    :return: List[string], e.g. ["etc", "I", "go" "to, "school", "I", "really", "like", "it"]
     """
-    pass
+    regex = r"(\()?(\b[^\s]+\b)((?<=\.\w).)?(\,)?(\:)?(\))?"
+
+    matches = re.findall(regex, text, re.MULTILINE)
+
+    return [m[1] for m in matches]
+
+
+if __name__ == '__main__':
+    text = "etc. I go to school. I really like it."
+
+    print(get_words_from_sentence(text))
